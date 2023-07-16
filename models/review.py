0176e6006451afdf896e3e78ac73d170ecb66ba7
root <lanremidelola@gmail.com>
2023-07-16 12:29:37 -0700

    alx

--- models/review.py
+++ models/review.py
@@ -4,15 +4,15 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-     """
+    """
         class Review:
             Attribute:
                 place_id : id(str)
                 user_id : user id(str)
                 text : str
-    """
+
+                """
 
     place_id = ""
     user_id = ""
     text = ""
-
